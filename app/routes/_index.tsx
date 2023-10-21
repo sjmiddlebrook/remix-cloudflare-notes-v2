d655a62fc699d37d7d6bb9278c630dda0290a39a
Jack Middlebrook <jack.middlebrook@gmail.com>
2023-10-21 16:15:34 +0800

    add tailwind and prettier

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,21 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction } from '@remix-run/cloudflare';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Notes Demo v2" },
-    { name: "description", content: "Notes app build with Remix and Cloudflare" },
+    { title: 'Notes Demo v2' },
+    {
+      name: 'description',
+      content: 'Notes app build with Remix and Cloudflare',
+    },
   ];
 };
 
 export default function Index() {
   return (
     <div>
-      <h1>Notes App</h1>
+      <h1 className="text-4xl text-blue-700">Notes App</h1>
       <p>
-        This is a demo app built with <a href="https://remix.run">Remix</a> and{" "}
+        This is a demo app built with <a href="https://remix.run">Remix</a> and{' '}
         <a href="https://www.cloudflare.com/">Cloudflare</a>.
       </p>
     </div>
