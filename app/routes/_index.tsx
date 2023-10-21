13660cb777523d4c2a2b10665d44e5dac6a9dae6
Jack Middlebrook <jack.middlebrook@gmail.com>
2023-10-21 15:55:40 +0800

    update home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,19 @@ import type { MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Notes Demo v2" },
+    { name: "description", content: "Notes app build with Remix and Cloudflare" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Notes App</h1>
+      <p>
+        This is a demo app built with <a href="https://remix.run">Remix</a> and{" "}
+        <a href="https://www.cloudflare.com/">Cloudflare</a>.
+      </p>
     </div>
   );
 }
